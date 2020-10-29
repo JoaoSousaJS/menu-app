f53606ef9240dc3e1298ec88871f8e74f8363c8e
Joao <joao.rsousa23@gmail.com>
2020-10-29 20:57:51 +1030

    feat: add style and icon to bottom tab

--- src/navigation/MealsNavigator.tsx
+++ src/navigation/MealsNavigator.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import { CategoriesScreen } from '../screens/categories/CategoriesScreen';
 import { CategoriesMealsScreen } from '../screens/categoryMeal/CategoryMealsScreen';
@@ -15,6 +16,7 @@ import { RootStackParamList } from '../types/stackNavigator/RouteParamList';
 import { optionsMealsScreen } from './ScreenOptions/MealsScreen';
 import { FavoritesScreen } from '../screens/favorites/FavoritesScreen';
 import { RootTabParamList } from '../types/tabNavigator/RouteParamList';
+import Color from '../constants/Color';
 
 const Stack = createStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator<RootTabParamList>();
@@ -60,9 +62,27 @@ export const MealsNavigatorStack = () => {
 
 export const MealsFavTabNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Meals" component={MealsNavigatorStack} />
-      <Tab.Screen name="Favorite" component={FavoritesScreen} />
+    <Tab.Navigator tabBarOptions={{ activeTintColor: Color.accentColor }}>
+      <Tab.Screen
+        name="Meals"
+        component={MealsNavigatorStack}
+        options={{
+          tabBarIcon: (tabInfo) => {
+            return (
+              <Ionicons name="ios-restaurant" size={25} color={tabInfo.color} />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={FavoritesScreen}
+        options={{
+          tabBarIcon: (tabInfo) => {
+            return <Ionicons name="ios-star" size={25} color={tabInfo.color} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
