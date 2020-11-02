23fc50dbfd7c2bbdbe41f2b8842ffdbef0303a3c
Joao <joao.rsousa23@gmail.com>
2020-11-02 14:29:34 +1030

    feat: add text if there is no favorite meal added yet.

--- src/screens/favorites/FavoritesScreen.tsx
+++ src/screens/favorites/FavoritesScreen.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
+import { DefaultText } from '../../components/DefaultText/DefaultText';
 import MealList from '../../components/MealList/MealList';
 import { IStateMeals } from '../../store/types/meals';
 import { CategoriesProps } from '../../types/stackNavigator/categories-screen-types';
@@ -11,5 +13,21 @@ export const FavoritesScreen = ({ navigation }: CategoriesProps) => {
     (state: IStateMeals) => state.meals.favoriteMeals,
   );
 
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No Favorite meals Found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
+
   return <MealList listData={favMeals} navigation={navigation} />;
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
