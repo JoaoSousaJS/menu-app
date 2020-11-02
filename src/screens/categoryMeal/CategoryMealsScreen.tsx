a67aa3d3111e07afc1aa50590f943f4e62bc39f3
Joao <joao.rsousa23@gmail.com>
2020-11-02 17:52:40 +1030

    feat: add text if there is no meals in the category screen

--- src/screens/categoryMeal/CategoryMealsScreen.tsx
+++ src/screens/categoryMeal/CategoryMealsScreen.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
+import { DefaultText } from '../../components/DefaultText/DefaultText';
 import MealList from '../../components/MealList/MealList';
 import { IStateMeals } from '../../store/types/meals';
 import { CategoriesProps } from '../../types/stackNavigator/categories-screen-types';
@@ -20,5 +22,21 @@ export const CategoriesMealsScreen = ({
     (meal) => meal.categoryIds.indexOf(categoryId) >= 0,
   );
 
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found, maybe check your filters?</DefaultText>
+      </View>
+    );
+  }
+
   return <MealList listData={displayedMeals} navigation={navigation} />;
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
