4869c4c833154f89caebca8235a8f58dd37abfea
Joao <joao.rsousa23@gmail.com>
2020-11-02 15:00:10 +1030

    feat: add filter logic to reducer

--- src/store/reducers/meals.ts
+++ src/store/reducers/meals.ts
@@ -1,5 +1,5 @@
 import { MEALS } from '../../data/dummy-data';
-import { TOGGLE_FAVORITE } from '../actions/meals';
+import { SET_FILTERS, TOGGLE_FAVORITE } from '../actions/meals';
 import { IAction } from '../types/action';
 
 const initialState = {
@@ -28,9 +28,27 @@ export const mealsReducer = (state = initialState, action: IAction) => {
         return { ...state, favoriteMeals: state.favoriteMeals.concat(meals) };
       }
 
+    case SET_FILTERS:
+      const appliedFilters = action.filters;
+      const updatedFilteredMeals = state.meals.filter((meal) => {
+        if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+          return false;
+        }
+        if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
+          return false;
+        }
+        if (appliedFilters.vegetarian && !meal.isVegetarian) {
+          return false;
+        }
+        if (appliedFilters.vegan && !meal.isVegan) {
+          return false;
+        }
+
+        return true;
+      });
+      return { ...state, filteredMeals: updatedFilteredMeals };
+
     default:
       return state;
   }
-
-  return state;
 };
--- src/store/types/action.ts
+++ src/store/types/action.ts
@@ -1,4 +1,10 @@
 export interface IAction {
   type: string;
   mealId: string;
+  filters: {
+    glutenFree: boolean;
+    lactoseFree: boolean;
+    vegetarian: boolean;
+    vegan: boolean;
+  };
 }
